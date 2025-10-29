fd5581480bdca1dbd38e276d6e9540b3241ca5ee
nflow24 <nate2flores@gmail.com>
2025-10-29 18:03:44 -0400

    nate

--- src/recipe.h
+++ src/recipe.h
@@ -7,12 +7,12 @@ using namespace std;
 class Recipe{
 private:
     int recipeNum;
-    string title;
-    string ingredientsExact;
+    string title; 
+    string ingredientsExact; // numbers of each ingredient (ex. 2 eggs)
     string instructions;
     string linkToRecipe;
     string source;
-    string ingredients;
+    string ingredients; // raw ingredients (ex. eggs)
 
 public:
     Recipe(int recipeNum, string title, string ingredientsExact, string instructions, string linktoRecipe, string source, string ingredients);

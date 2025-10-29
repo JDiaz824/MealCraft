420dbc57f45d24c8c7850f8e7a8c4aee30855f12
nflow24 <nate2flores@gmail.com>
2025-10-29 19:33:47 -0400

    started hashmap class

--- src/data-structures/hashMap.h
+++ src/data-structures/hashMap.h
@@ -1,1 +1,13 @@
 //Hash Map-based Inverted Index data structure, using polymorphism
+
+#pragma once
+
+#include "src/invertedIndex.h"
+
+using namespace std;
+
+// Entry struct for individual key-value pair
+struct Entry {
+    string key;
+    vector<Recipe> recipes;
+}

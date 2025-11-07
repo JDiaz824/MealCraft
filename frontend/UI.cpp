2b924e9401618e5ed43d730ef82dd42026bdaa6a
nflow24 <nate2flores@gmail.com>
2025-11-07 18:47:03 -0500

    made initial window prototype

--- frontend/UI.cpp
+++ frontend/UI.cpp
@@ -1,11 +1,20 @@
 #include "UI.h"
+#include <SFML/Graphics.hpp>  // Include SFML classes
+using namespace std;
 
-// Constructor — NO return type
+// Constructor
 UI::UI()
-: m_window(sf::VideoMode(800, 600), "MealCraft"), m_shape(100.f)
+: width(1500), height(1000), m_window(sf::VideoMode(width, height), "MealCraft")
 {
-    m_shape.setFillColor(sf::Color::Green);
+    // Load font once
+    if (!m_font.loadFromFile("../frontend/assets/regular.otf")) {
+        std::cerr << "Failed to load font!" << std::endl;
+    }
+
+    makeText(text, "Welcome to MealCraft!", m_font, 100, sf::Color::White);
+    setText(text, width / 2,(height / 2) - 75);
 }
+
 
 void UI::run() {
     while (m_window.isOpen()) {
@@ -29,6 +38,24 @@ void UI::update() {
 
 void UI::render() {
     m_window.clear();
-    m_window.draw(m_shape);
+    m_window.draw(text);
     m_window.display();
 }
+
+void UI::setText(sf::Text &text, float x, float y){
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width/2.0f,
+                   textRect.top + textRect.height/2.0f);
+    text.setPosition(sf::Vector2f(x, y));
+};
+
+void UI::makeText(sf::Text &text, string str, sf::Font &font, int fontSize, sf::Color color) {
+    // sf::Uint32 integer type allows for a combo of one or more styles
+    //  -- https://www.sfml-dev.org/documentation/2.0/classsf_1_1Text.php
+    // sets font - same font is used for all text
+    // declare text object, set string, font, characterSize, color, and style(s)
+    text.setString(str);
+    text.setFont(font);
+    text.setCharacterSize(fontSize);
+    text.setFillColor(color);
+};
--- frontend/UI.h
+++ frontend/UI.h
@@ -1,17 +1,27 @@
-#pragma once
+// UI class for frontend
 
+#pragma once
+#include <iostream>
+#include <fstream>
 #include <SFML/Graphics.hpp>  // Include SFML classes
+using namespace std;
 
 class UI {
 public:
-    UI();            // Constructor
+    UI();             // Constructor
     void run();       // Start the loop
 
 private:
+    int width, height;
     sf::RenderWindow m_window;
-    sf::CircleShape m_shape;
+    sf::Font m_font;
+    sf::Text text;
 
     void processEvents();
     void update();
     void render();
+
+    void setText(sf::Text& text, float x, float y);
+    void makeText(sf::Text& text, string str, sf::Font& font, int fontSize, sf::Color color);
+    
 };

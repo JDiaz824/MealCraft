309304f9affda62d2594b96cd61e8ae6a01182d5
nflow24 <nate2flores@gmail.com>
2025-11-08 03:36:36 -0500

    git told me to commit so i did

--- frontend/UI.cpp
+++ frontend/UI.cpp
@@ -2,58 +2,93 @@
 #include <SFML/Graphics.hpp>  // Include SFML classes
 using namespace std;
 
-// Constructor
-UI::UI()
-: width(1500), height(1000), m_window(sf::VideoMode(width, height), "MealCraft")
-{
-    // Load font once
-    if (!m_font.loadFromFile("../frontend/assets/regular.otf")) {
-        std::cerr << "Failed to load font!" << std::endl;
-    }
+/**
+ * Constructor
+ */
+UI::UI() : width(1500), height(1000), window(sf::VideoMode(width, height), "MealCraft") {
 
-    makeText(text, "Welcome to MealCraft!", m_font, 100, sf::Color::White);
-    setText(text, width / 2,(height / 2) - 75);
+    if (!reg_font.loadFromFile("../frontend/assets/regular.otf")) {
+        cerr << "Failed to load regular font!" << endl;
+    }
+    if (!bold_font.loadFromFile("../frontend/assets/bold.otf")) {
+        cerr << "Failed to load bold font!" << endl;
+    }
+    if (!italic_font.loadFromFile("../frontend/assets/italic.otf")) {
+        cerr << "Failed to load italic font!" << endl;
+    }
+    if (!strong_font.loadFromFile("../frontend/assets/bold-italic.otf")) {
+        cerr << "Failed to load strong font!" << endl;
+    }        
+
+    makeText(title, "MealCraft!", reg_font, 200, sf::Color::White);
+    setText(title, width / 2,(height / 2) - 400);
+
+    // makeText(t1, "Welcome to MealCraft!", reg_font, 50, sf::Color::White);
+    // setText(t1, width / 2,(height / 2) - 125);
+
+    // makeText(t2, "Welcome to MealCraft!", italic_font, 50, sf::Color::White);
+    // setText(t2, width / 2,(height / 2) - 75);
+    
+    // makeText(t3, "Welcome to MealCraft!", strong_font, 50, sf::Color::White);
+    // setText(t3, width / 2,(height / 2) - 25);
 }
 
-
+/**
+ * "main" function that combines event handling, backend logic, and rendering UI
+ */
 void UI::run() {
-    while (m_window.isOpen()) {
+    while (window.isOpen()) {
         processEvents();
         update();
         render();
     }
 }
 
+/**
+ * Processes user input and window events (ex. keyboard/mouse inputs)
+ */
 void UI::processEvents() {
     sf::Event event;
-    while (m_window.pollEvent(event)) {
+    while (window.pollEvent(event)) {
         if (event.type == sf::Event::Closed)
-            m_window.close();
+            window.close();
     }
 }
 
+/**
+ * Updates logic (recipe searching)
+ */
 void UI::update() {
     // Update logic (currently empty)
 }
 
+/**
+ * Handles rendering UI
+ */
 void UI::render() {
-    m_window.clear();
-    m_window.draw(text);
-    m_window.display();
+    window.clear();
+    window.draw(title);
+    window.draw(t1);
+    window.draw(t2);
+    window.draw(t3);
+    window.display();
 }
 
-void UI::setText(sf::Text &text, float x, float y){
+
+/**
+ * Positions Text
+ */
+void UI::setText(sf::Text &text, float x, float y) {
     sf::FloatRect textRect = text.getLocalBounds();
     text.setOrigin(textRect.left + textRect.width/2.0f,
                    textRect.top + textRect.height/2.0f);
     text.setPosition(sf::Vector2f(x, y));
 };
 
+/**
+ * Declare text object, string, font, font size, and color
+ */
 void UI::makeText(sf::Text &text, string str, sf::Font &font, int fontSize, sf::Color color) {
-    // sf::Uint32 integer type allows for a combo of one or more styles
-    //  -- https://www.sfml-dev.org/documentation/2.0/classsf_1_1Text.php
-    // sets font - same font is used for all text
-    // declare text object, set string, font, characterSize, color, and style(s)
     text.setString(str);
     text.setFont(font);
     text.setCharacterSize(fontSize);
--- frontend/UI.h
+++ frontend/UI.h
@@ -8,19 +8,27 @@ using namespace std;
 
 class UI {
 public:
-    UI();             // Constructor
-    void run();       // Start the loop
+    UI();     
+    void run();
 
 private:
     int width, height;
-    sf::RenderWindow m_window;
-    sf::Font m_font;
-    sf::Text text;
+    sf::RenderWindow window;
+    sf::Font reg_font;
+    sf::Font bold_font;
+    sf::Font italic_font;
+    sf::Font strong_font;
+    sf::Text title;
+    sf::Text t1;
+    sf::Text t2;
+    sf::Text t3;
+
 
     void processEvents();
     void update();
     void render();
 
+    // functions below are reused from my minesweeper project from Prog 2
     void setText(sf::Text& text, float x, float y);
     void makeText(sf::Text& text, string str, sf::Font& font, int fontSize, sf::Color color);
     
